93fe4fde65f7618029a12d9815e506378c7a2549
Karim Raafat <karimahmedraafat2004@gmail.com>
2025-04-06 01:04:17 +0200

    Add missing implementations in Table and Page classes

--- Starter Code/Starter Code/src/DBMS/Table.java
+++ Starter Code/Starter Code/src/DBMS/Table.java
@@ -2,9 +2,17 @@ package DBMS;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Arrays;;
 
-public class Table implements Serializable
-{
-	
+public class Table implements Serializable {
+    String tableName;
+    String[] columnNames;
+    ArrayList<Page> pages;
+
+    public Table(String tableName, String[] columnsNames) {
+        this.tableName = tableName;
+        this.columnNames = columnsNames;
+        pages = new ArrayList<Page>();
+    }
+
 }
