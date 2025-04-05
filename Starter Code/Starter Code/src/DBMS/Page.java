a6d05cd1300fa305288604420d9220bf5722f38d
Karim Raafat <karimahmedraafat2004@gmail.com>
2025-04-06 01:06:18 +0200

    Update Page.java

--- Starter Code/Starter Code/src/DBMS/Page.java
+++ Starter Code/Starter Code/src/DBMS/Page.java
@@ -3,7 +3,25 @@ package DBMS;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Page implements Serializable
-{
-	
+public class Page implements Serializable {
+    int pageNumber;
+    ArrayList<String[]> records;
+
+    public Page(int pageNumber) {
+        this.pageNumber = pageNumber;
+        records = new ArrayList<String[]>();
+    }
+
+    public boolean isFull() {
+        return records.size() >= DBApp.dataPageSize;
+    }
+
+    public void addRecord(String[] record) {
+        records.add(record);
+    }
+
+    public ArrayList<String[]> getAllRecords() {
+        return records;
+    }
+
 }
